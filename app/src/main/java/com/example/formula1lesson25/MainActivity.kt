2e0acc1520159927ad2f0bd953ac1e6ff40503a6
User <taranovegor91@gmail.com>
2022-01-31 15:29:43 +0300

    убрал из приложения квадрат(перенес в другое приложение).Исправил замечания. Исправил логику кнопки старт (запретил возможность множественного стартования без нажатия кнопки сброс).

--- app/src/main/java/com/example/formula1lesson25/MainActivity.kt
+++ app/src/main/java/com/example/formula1lesson25/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.formula1lesson25
 
-import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.TranslateAnimation
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.coroutines.*
-import kotlin.concurrent.thread
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private val scope = CoroutineScope(Dispatchers.IO)
@@ -22,13 +17,10 @@ class MainActivity : AppCompatActivity() {
     var rand1: Long = 0
     var rand2: Long = 0
     var rand3: Long = 0
+    var sbrosBool = false
+    var startBool = true
     lateinit var buttonStart: Button
-    lateinit var cvadr: LinearLayout
-    lateinit var cLayout: ConstraintLayout
-    lateinit var button2: Button
-
-
-    lateinit var btnSbrosCvadr: Button
+    lateinit var buttonSbros: Button
     lateinit var imageView: ImageView
     lateinit var imageView2: ImageView
     lateinit var imageView3: ImageView
@@ -36,151 +28,82 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonStart = findViewById(R.id.buttonStart)
-        button2 = findViewById(R.id.button2)
-
-        btnSbrosCvadr = findViewById(R.id.btnSbrosCvadr)
+        buttonSbros = findViewById(R.id.button2)
 
         imageView = findViewById(R.id.imageView)
         imageView2 = findViewById(R.id.imageView2)
         imageView3 = findViewById(R.id.imageView3)
-        cvadr = findViewById(R.id.cvadr)
-        cLayout = findViewById(R.id.cLayout)
 
         buttonStart.setOnClickListener {
-            rand1 = (1000..8000).random().toLong()
-            rand2 = (1000..8000).random().toLong()
-            rand3 = (1000..8000).random().toLong()
-            imageView.startAnimation(TranslateAnimation(0f, 0f,
-                0f, -1200f)
-                .apply {
-                    duration = rand1
-                    fillAfter = true
-                })
-            imageView2.startAnimation(TranslateAnimation(0f, 0f,
-                0f, -1200f)
-                .apply {
-                    duration = rand2
-                    fillAfter = true
-                })
-            imageView3.startAnimation(TranslateAnimation(0f, 0f,
-                0f, -1200f)
-                .apply {
-                    duration = rand3
-                    fillAfter = true
-                })
 
+            if (startBool) {
+                rand1 = (1000..8000).random().toLong()
+                rand2 = (1000..8000).random().toLong()
+                rand3 = (1000..8000).random().toLong()
+                imageView.startAnimation(TranslateAnimation(0f, 0f,
+                    0f, -1200f)
+                    .apply {
+                        duration = rand1
+                        fillAfter = true
+                    })
+                imageView2.startAnimation(TranslateAnimation(0f, 0f,
+                    0f, -1200f)
+                    .apply {
+                        duration = rand2
+                        fillAfter = true
+                    })
+                imageView3.startAnimation(TranslateAnimation(0f, 0f,
+                    0f, -1200f)
+                    .apply {
+                        duration = rand3
+                        fillAfter = true
+                    })
 
+                sbrosBool = false
 
-            if (rand1 < rand2 && rand1 < rand3) {
-                Run.after(rand1) {
-                    Toast.makeText(this,
-                        "Победил первый Самолет, для формулы1 - это чудо, я считаю",
-                        Toast.LENGTH_LONG).show()
-                }
-            } else if (rand1 > rand2 && rand2 < rand3) {
-                Run.after(rand2) {
-                    Toast.makeText(this,
-                        "Победил второй Самолет, для формулы1 - это чудо, я считаю",
-                        Toast.LENGTH_LONG).show()
-                }
-            } else if (rand1 > rand3 && rand2 > rand3) {
-                Run.after(rand3) {
-                    Toast.makeText(this,
-                        "Победил третий Самолет, для формулы1 - это чудо, я считаю",
-                        Toast.LENGTH_LONG).show()
+                if (rand1 < rand2 && rand1 < rand3) {
+                    Run.after(rand1) {
+                        if (!sbrosBool ) {
+                            Toast.makeText(this,
+                                "Победил первый Самолет, для формулы1 - это чудо, я считаю",
+                                Toast.LENGTH_LONG).show()
+                        }
+                    }
+                } else if (rand1 > rand2 && rand2 < rand3) {
+                    Run.after(rand2) {
+                        if (!sbrosBool ) {
+                            Toast.makeText(this,
+                                "Победил второй Самолет, для формулы1 - это чудо, я считаю",
+                                Toast.LENGTH_LONG).show()
+                        }
+                    }
+                } else if (rand1 > rand3 && rand2 > rand3) {
+                    Run.after(rand3) {
+                        if (!sbrosBool ) {
+                            Toast.makeText(this,
+                                "Победил третий Самолет, для формулы1 - это чудо, я считаю",
+                                Toast.LENGTH_LONG).show()
+                        }
+                    }
+                } else if (rand1 == rand3 || rand1 == rand2 || rand2 == rand3) {
+                    Run.after(rand3) {
+                        if (!sbrosBool ) {
+                            Toast.makeText(this,
+                                "Не вероятно, кто-то пришел ноздря в ноздрю!!! - это чудо, я считаю, но нужно повторить старт!",
+                                Toast.LENGTH_LONG).show()
+                        }
+                    }
                 }
             }
+            startBool=false
         }
-        button2.setOnClickListener {
-
+        buttonSbros.setOnClickListener {
+            sbrosBool = true
+            startBool = true
             imageView.clearAnimation()
             imageView2.clearAnimation()
             imageView3.clearAnimation()
 
-        }
-
-        var bul = true
-        var valueX = 0f
-        var valueY = 0f
-        cvadr.setOnClickListener {
-            bul=true
-            job = scope.launch {
-                while (bul) {
-                    if (cvadr.x<cLayout.width-cvadr.width){
-                    while (cvadr.x < cLayout.width - cvadr.width) {
-                        delay(50)
-                        valueX = valueX + 10f
-                        valueY = valueY + 10f
-                        withContext(Dispatchers.Main) {
-                            cvadr.translationX = valueX
-                            cvadr.translationY = -valueY
-                        }
-                    }
-                    }else{
-                        while (cvadr.x > 0&&cvadr.y>cLayout.height-cvadr.height) {
-                            delay(50)
-                            valueX = valueX - 10f
-                            valueY = valueY + 10f
-                            withContext(Dispatchers.Main) {
-                                cvadr.translationX = valueX
-                                cvadr.translationY = -valueY
-                            }
-                        }
-                    }
-
-                    while (cvadr.y > 0f && cvadr.x > 0) {
-                        delay(50)
-                        valueX = valueX - 10f
-                        valueY = valueY + 10f
-                        withContext(Dispatchers.Main) {
-                            cvadr.translationX = valueX
-                            cvadr.translationY = -valueY
-                        }
-                    }
-                    if (cvadr.x > 0f) {
-                        while (cvadr.x > 0f) {
-                            delay(50)
-                            valueX = valueX - 10f
-                            valueY = valueY - 10f
-                            withContext(Dispatchers.Main) {
-                                cvadr.translationX = valueX
-                                cvadr.translationY = -valueY
-                            }
-                        }
-                    } else {
-                        while (cvadr.y > 0f && cvadr.x < cLayout.width) {
-                            delay(50)
-                            valueX = valueX + 10f
-                            valueY = valueY + 10f
-                            withContext(Dispatchers.Main) {
-                                cvadr.translationX = valueX
-                                cvadr.translationY = -valueY
-                            }
-                        }
-                    }
-                    while (cvadr.y < cLayout.height - cvadr.height && cvadr.x < cLayout.width - cvadr.height) {
-                        delay(50)
-                        valueX = valueX + 10f
-                        valueY = valueY - 15f
-                        withContext(Dispatchers.Main) {
-                            cvadr.translationX = valueX
-                            cvadr.translationY = -valueY
-                        }
-
-                    }
-
-                }
-            }
-
-
-        }
-        btnSbrosCvadr.setOnClickListener {
-            job.cancel()
-            bul=false
-            cvadr.translationX = 0f
-            cvadr.translationY = 0f
-            valueX=0f
-            valueY=0f
         }
 
 
